f207c8cdc1a8fa325c438047e5d3479a4cd15b59
sammy58492 <kurellasumanth@hotmail.com>
2017-04-04 11:32:43 +0530

    servi

---
+++ Schat/Services/SChatServices.API/SChat.Business/Services/SChatService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SChat.Models;
+using SChat.DataAccess;
+
+namespace SChat.Business
+{
+  public   class SChatService:ISChatService
+    {
+        public ConversationResponse GetConversationList(int SenderID, int ReceiverID)
+        {
+            ConversationResponse itemListResponse = new ConversationResponse();
+            List<RetriveMessages> itemList = new List<RetriveMessages>();
+
+            IChatDBManager itemDBManager = new ChatDBManager();
+
+            IList<MyMessagesResult> wineResults = itemDBManager.GetMessageList(SenderID, ReceiverID).ToList();
+            foreach (MyMessagesResult result in wineResults)
+            {
+                itemList.Add(new RetriveMessages
+                {
+                    Conversation = result.message
+                });
+            }
+            itemListResponse.ConversationList = itemList;
+            return itemListResponse;
+        }
+    }
+}
---
+++ Schat/Services/SChatServices.API/SChatServices.API/Controllers/SCHATController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SChat.Models;
+using SChat.Business;
+using System.Web.Http;
+
+
+namespace SChatServices.API.Controllers
+{
+    public class SCHATController : ApiController
+    {
+        // GET: SCHAT
+       
+
+        [HttpGet]
+        public ConversationResponse ConversationList(int objectId,int userid)
+        {
+            ConversationResponse resp = new ConversationResponse();
+            ISChatService itemService = new SChatService();
+            resp = itemService.GetConversationList(objectId,userid);
+            return resp;
+        }
+    }
+}
